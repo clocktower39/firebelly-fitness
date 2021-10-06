cc343e1cd8532339ccb4b24b9239ede5550164a1
Matt Kearns <clocktower39@aol.com>
2021-10-05 18:57:45 -0700

    udpate note when selectedDate changes

--- firebelly-client/src/Components/DailyView/Note.js
+++ firebelly-client/src/Components/DailyView/Note.js
@@ -1,65 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { 
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Button,
-    Grid,
-    TextField,
-    Typography,
-    makeStyles
-} from '@material-ui/core';
-import { ExpandMore } from '@material-ui/icons';
 import {
-    requestDailyNote,
-    updateDailyNote,
-} from "../../Redux/actions";
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+import { requestDailyNote, updateDailyNote } from "../../Redux/actions";
 
-const useStyles = makeStyles(theme => ({
-    root: {},
-}))
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
 
 export default function Note(props) {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user);
-    const today = useSelector((state) => state.calander.dailyView);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const today = useSelector((state) => state.calander.dailyView);
 
-    const [note, setNote] = useState();
-    const handleChange = (e) => {
-        setNote(e.target.value)
-    }
+  const [note, setNote] = useState();
+  const handleChange = (e) => {
+    setNote(e.target.value);
+  };
 
-    const handleSave = () => {
-        dispatch(updateDailyNote({_id:today.dailyNote._id, note}))
-    }
+  const handleSave = () => {
+    dispatch(updateDailyNote({ _id: today.dailyNote._id, note }));
+  };
 
-    useEffect(()=> {
-        setNote(today.dailyNote.note)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[today.dailyNote])
+  useEffect(() => {
+    setNote(today.dailyNote.note);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [today.dailyNote]);
 
-    useEffect(()=> {
-        dispatch(requestDailyNote(user._id, props.selectedDate))
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[props.selectedDate])
+  useEffect(() => {
+    setNote("");
+    dispatch(requestDailyNote(user._id, props.selectedDate));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.selectedDate]);
 
-    return (
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMore />}
-            >
-                <Grid container alignItems="center">
-                    <Grid item xs={3}><Typography className={classes.heading}>Notes</Typography></Grid>
-                </Grid>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} ><TextField fullWidth variant="outlined" value={note} onChange={(e)=>handleChange(e)} label="Please provide feedback on your day; what was difficult and what went well?"/></Grid>
-                    <Grid item container justifyContent="center" xs={12} ><Button variant="outlined" onClick={handleSave} >Save</Button></Grid>
-                </Grid>
-            </AccordionDetails>
-        </Accordion>
-    )
+  return (
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMore />}>
+        <Grid container alignItems="center">
+          <Grid item xs={3}>
+            <Typography className={classes.heading}>Notes</Typography>
+          </Grid>
+        </Grid>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              value={note||''}
+              onChange={(e) => handleChange(e)}
+              label="Please provide feedback on your day; what was difficult and what went well?"
+            />
+          </Grid>
+          <Grid item container justifyContent="center" xs={12}>
+            <Button variant="outlined" onClick={handleSave}>
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
