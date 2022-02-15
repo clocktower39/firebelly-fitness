bf8dbc044852b7bef63495af12c07e9e24b42673
clocktower39 <clocktower39@aol.com>
2022-02-15 02:00:34 -0700

    updated notes component

--- firebelly-client/src/Components/ActivityTrackers/Notes.js
+++ firebelly-client/src/Components/ActivityTrackers/Notes.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 import { requestNotes, createNote } from "../../Redux/actions";
 import AuthNavbar from "../AuthNavbar";
 
@@ -24,22 +25,32 @@ export default function Notes() {
   return (
     <>
       <Container maxWidth="md" sx={{ height: "100%", paddingTop: "15px", paddingBottom: "75px" }}>
-        <Paper sx={{ padding: "0px 15px", borderRadius: "15px", minHeight: "100%" }}>
+        <Paper sx={{ padding: "5px 15px", borderRadius: "15px", minHeight: "100%" }}>
           <Grid container sx={{ alignItems: "center", paddingBottom: "15px" }}>
-            <Grid item xs={3}>
-              <Typography variant="h4">Notes</Typography>
+            <Grid item xs={12}>
+              <Typography variant="h4" textAlign="center">
+                Notes
+              </Typography>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
             <Grid container item xs={12}>
               {notes &&
                 notes.map((n, i) => (
-                  <Grid key={`note-${n.date}-${i}`} container>
-                    <Grid item sm={2} xs={4}>
-                      {n.date.substr(0, 10)}
+                  <Grid key={`note-${n.date}-${i}`} container sx={{ padding: "12px 0px" }}>
+                    <Grid container item xs={2} sx={{ justifyContent: 'center', alignItems: 'center', }}>
+                      <AccountCircle />
                     </Grid>
-                    <Grid item sm={10} xs={8}>
-                      {n.note}
+                    <Grid container item xs={10}>
+                      <Grid container item xs={12}>
+                        <Typography variant="body1">
+                          {n.firstName} {n.lastName}
+                        </Typography>
+                        <Typography variant="caption" component="p" sx={{ padding: '2.5px 5px',}}>{n.date.substr(0, 10)}</Typography>
+                      </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant="body2">{n.note}</Typography>
+                    </Grid>
                     </Grid>
                   </Grid>
                 ))}
