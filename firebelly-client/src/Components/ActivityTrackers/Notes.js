f50c6e83453a2af329ece3f477acbaeb30dd6599
clocktower39 <clocktower39@aol.com>
2022-02-27 16:59:52 -0700

    TextField endAdornment submit button, flex body with overflowY for messages only

--- firebelly-client/src/Components/ActivityTrackers/Notes.js
+++ firebelly-client/src/Components/ActivityTrackers/Notes.js
@@ -24,53 +24,53 @@ export default function Notes() {
 
   return (
     <>
-      <Container maxWidth="md" sx={{ height: "100%", paddingTop: "15px", paddingBottom: "75px" }}>
-        <Paper sx={{ padding: "5px 15px", borderRadius: "15px", minHeight: "100%" }}>
-          <Grid container sx={{ alignItems: "center", paddingBottom: "15px" }}>
-            <Grid item xs={12}>
-              <Typography variant="h4" textAlign="center">
-                Notes
-              </Typography>
-            </Grid>
+      <Container maxWidth="md" sx={{ height: "100%", padding: "15px 0px" }}>
+        <Paper sx={{ padding: "5px 15px", borderRadius: "15px", height: "100%", display: 'flex', flexDirection: 'column', flex: 'auto',  }}>
+
+          <Grid container item xs={12} sx={{ justifyContent: 'center', paddingBottom: "15px", alignSelf: 'flex-start', flex: 'initial', }}>
+            <Typography variant="h4">
+              Notes
+            </Typography>
           </Grid>
-          <Grid container spacing={2}>
-            <Grid container item xs={12}>
-              {notes &&
-                notes.map((n, i) => (
-                  <Grid key={`note-${n.date}-${i}`} container sx={{ padding: "12px 0px" }}>
-                    <Grid container item xs={2} sx={{ justifyContent: 'center', alignItems: 'center', }}>
-                      <AccountCircle />
+
+          <Grid container item xs={12} sx={{ alignSelf: 'flex-start', alignContent: 'flex-start', overflowY: 'scroll', flex: 'auto', }}>
+            {notes &&
+              notes.map((n, i) => (
+                <Grid key={`note-${n.date}-${i}`} container sx={{ padding: "12px 0px" }}>
+                  <Grid container item xs={2} sx={{ justifyContent: 'center', alignItems: 'center', }}>
+                    <AccountCircle />
+                  </Grid>
+                  <Grid container item xs={10}>
+                    <Grid container item xs={12}>
+                      <Typography variant="body1">
+                        {n.firstName} {n.lastName}
+                      </Typography>
+                      <Typography variant="caption" component="p" sx={{ padding: '2.5px 5px', }}>{n.date.substr(0, 10)}</Typography>
                     </Grid>
-                    <Grid container item xs={10}>
-                      <Grid container item xs={12}>
-                        <Typography variant="body1">
-                          {n.firstName} {n.lastName}
-                        </Typography>
-                        <Typography variant="caption" component="p" sx={{ padding: '2.5px 5px',}}>{n.date.substr(0, 10)}</Typography>
-                      </Grid>
                     <Grid item xs={12}>
                       <Typography variant="body2">{n.note}</Typography>
                     </Grid>
-                    </Grid>
                   </Grid>
-                ))}
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                multiline
-                variant="outlined"
-                value={note || ""}
-                onChange={(e) => handleChange(e)}
-                label="Note"
-              />
-            </Grid>
-            <Grid item container style={{ justifyContent: "center" }} xs={12}>
-              <Button variant="outlined" onClick={handleSave}>
-                Submit
-              </Button>
-            </Grid>
+                </Grid>
+              ))}
           </Grid>
+
+          <Grid container item xs={12} sx={{ flexGrow: 1, alignContent: 'flex-end', flex: 'initial', }}>
+            <TextField
+              fullWidth
+              multiline
+
+              variant="outlined"
+              value={note || ""}
+              onChange={(e) => handleChange(e)}
+              label="Note"
+              InputProps={{
+                endAdornment: <Button variant="contained" sx={{}} onClick={handleSave}>Submit</Button>
+              }}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+
         </Paper>
       </Container>
       <AuthNavbar />
