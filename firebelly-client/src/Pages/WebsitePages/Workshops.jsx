16d1d6fd1c21e16de986a374fb6c4d2f719e9f32
clocktower39 <clocktower39@aol.com>
2025-08-01 14:04:57 -0700

    new website home: workshops

--- firebelly-client/src/Pages/WebsitePages/Workshops.jsx
+++ firebelly-client/src/Pages/WebsitePages/Workshops.jsx
@@ -6,32 +6,29 @@ import { Container, List, ListItem, Paper, Typography } from "@mui/material";
 export default function Workshops() {
   return (
     <>
-      <WebsiteNavbar />
-      <Container>
-        <Paper sx={{ margin: "15px 0", padding: "5px 25px" }}>
-          <Typography variant="h4" textAlign="center" gutterBottom sx={{ fontFamily: 'Montserrat'}}>
-            Workshops
-          </Typography>
-          <Typography sx={{ fontFamily: "Source Sans Pro" }}>
-            Workshops are used to educate proper form & movement mechanics for certain exercises and
-            skills. This will be very useful for those that may not want one-on-one training, but
-            are having problems with a particular lift.
-          </Typography>
-          <Typography sx={{ fontFamily: "Source Sans Pro" }}>These will include many movements such as:</Typography>
-          <List sx={{ listStyleType: "disc", pl: 4, fontFamily: "Source Sans Pro" }}>
-            <ListItem sx={{ display: "list-item", fontFamily: 'inherit', }}>
-              Common Exercises (Squats, Deadlifts, Bench Press, & Overhead Press)
-            </ListItem>
-            <ListItem sx={{ display: "list-item", fontFamily: 'inherit', }}>Olympic lifts (Snatch, Clean & Jerk)</ListItem>
-            <ListItem sx={{ display: "list-item", fontFamily: 'inherit', }}>
-              Calisthenics (Handstands, Human Flagpoles, Levers, Muscle-ups, Planches)
-            </ListItem>
-            <ListItem sx={{ display: "list-item", fontFamily: 'inherit', }}>Stretching</ListItem>
-          </List>
-        </Paper>
-      </Container>
-      <div style={{ height: "20px" }} />
-      <Footer />
+      <Typography variant="h4" textAlign="center" gutterBottom sx={{ fontFamily: "Montserrat" }}>
+        Workshops
+      </Typography>
+      <Typography sx={{ fontFamily: "Source Sans Pro" }}>
+        Workshops are used to educate proper form & movement mechanics for certain exercises and
+        skills. This will be very useful for those that may not want one-on-one training, but are
+        having problems with a particular lift.
+      </Typography>
+      <Typography sx={{ fontFamily: "Source Sans Pro" }}>
+        These will include many movements such as:
+      </Typography>
+      <List sx={{ listStyleType: "disc", pl: 4, fontFamily: "Source Sans Pro" }}>
+        <ListItem sx={{ display: "list-item", fontFamily: "inherit" }}>
+          Common Exercises (Squats, Deadlifts, Bench Press, & Overhead Press)
+        </ListItem>
+        <ListItem sx={{ display: "list-item", fontFamily: "inherit" }}>
+          Olympic lifts (Snatch, Clean & Jerk)
+        </ListItem>
+        <ListItem sx={{ display: "list-item", fontFamily: "inherit" }}>
+          Calisthenics (Handstands, Human Flagpoles, Levers, Muscle-ups, Planches)
+        </ListItem>
+        <ListItem sx={{ display: "list-item", fontFamily: "inherit" }}>Stretching</ListItem>
+      </List>
     </>
   );
 }
