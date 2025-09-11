6a711ffed43c8f086db7122a5cee7be2fc7de6fc
Matt <clocktower39@aol.com>
2025-09-11 14:02:53 -0700

    FeedbackWorkoutInput

--- firebelly-client/src/Components/TrainingComponents/SwipeableSet.jsx
+++ firebelly-client/src/Components/TrainingComponents/SwipeableSet.jsx
@@ -15,14 +15,10 @@ import {
   FormControlLabel,
   Grid,
   IconButton,
-  List,
-  ListItem,
-  ListItemText,
   MobileStepper,
   Paper,
   Radio,
   RadioGroup,
-  Slide,
   TextField,
   Toolbar,
   Tooltip,
@@ -99,22 +95,6 @@ function SwipeableSet(props) {
 
   const handleWorkoutCompleteCheckbox = () => setWorkoutCompleteStatus((prev) => !prev);
 
-  const handleFeedbackKeyDown = (e) => {
-    // Check if any of Enter, Shift+Enter, Backspace, or Delete is pressed to resize
-    if (
-      (e.key === "Enter" && !e.shiftKey) || // Regular Enter
-      (e.key === "Enter" && e.shiftKey) || // Shift + Enter
-      e.key === "Backspace" || // Backspace
-      e.key === "Delete" // Delete
-    ) {
-      setHeightToggle((prev) => !prev);
-    }
-  };
-
-  const handleFeedbackChange = (e) => {
-    setWorkoutFeedback(e.target.value);
-  };
-
   const allExercises = useMemo(() => {
     return localTraining.flatMap((group) => group.map((ex) => ex));
   }, [localTraining]);
@@ -157,7 +137,7 @@ function SwipeableSet(props) {
   }, [toggleNewSet]);
 
   useEffect(() => {
-    handleStepChange(0);
+    handleStepChange(2);
   }, [selectedDate]);
 
   return (
@@ -253,28 +233,29 @@ function SwipeableSet(props) {
                 <Grid container size={12} sx={{ justifyContent: "center" }}>
                   <Typography variant="body1">Feedback:</Typography>
                 </Grid>
-                <Grid container size={12} sx={{ justifyContent: "center", alignItems: "center" }}>
-                  <IconButton onClick={handleFeedbackDialogOpen} >
-                    <AddCircle />
-                  </IconButton>
-                  Choose Exercises
-                </Grid>
-                <Grid container size={12} sx={{ padding: "0px 25px" }}>
-                  {feedbackSelectedExercises.map((exercise) => {
-                    return <FeedbackExerciseInput exercise={exercise} />;
-                  })}
-                </Grid>
                 <Grid container size={12} sx={{ padding: "30px 0" }}>
-                  <TextField
-                    label="Overall Feedback (Disabled, working out final details)"
+                  <FeedbackWorkoutInput workoutFeedback={workoutFeedback} setWorkoutFeedback={setWorkoutFeedback} />
+                  {/* <TextField
+                    label="Overall Feedback"
                     value={workoutFeedback}
                     fullWidth
                     multiline
                     minRows={5}
                     onKeyDown={handleFeedbackKeyDown}
                     onChange={handleFeedbackChange}
-                  />
+                  /> */}
                 </Grid>
+                {/* <Grid container size={12} sx={{ justifyContent: "center", alignItems: "center" }}>
+                  <IconButton onClick={handleFeedbackDialogOpen} >
+                    <AddCircle />
+                  </IconButton>
+                  Choose Exercises
+                </Grid> */}
+                {/* <Grid container size={12} sx={{ padding: "0px 25px" }}>
+                  {feedbackSelectedExercises.map((exercise) => {
+                    return <FeedbackExerciseInput exercise={exercise} />;
+                  })}
+                </Grid> */}
               </Grid>
             </Grid>
           </Grid>
@@ -330,6 +311,185 @@ function SwipeableSet(props) {
     </Box>
   );
 }
+const FeedbackWorkoutInput = ({ workoutFeedback, setWorkoutFeedback, }) => {
+  const user = useSelector((state) => state.user);
+  const [feedbackDifficulty, setFeedbackDifficulty] = useState(Number(workoutFeedback.difficulty) || 1);
+
+  const handleFeedbackDifficultyChange = (event) => {
+    const newValue = Number(event.target.value);
+    setFeedbackDifficulty(newValue);
+    setWorkoutFeedback((prev) => ({
+      ...prev,
+      difficulty: newValue,
+    }));
+  };
+
+  const [commentText, setCommentText] = useState("");
+
+  const handleFeedbackKeyDown = (e) => {
+    // Check if any of Enter, Shift+Enter, Backspace, or Delete is pressed to resize
+    if (
+      (e.key === "Enter" && !e.shiftKey) || // Regular Enter
+      (e.key === "Enter" && e.shiftKey) || // Shift + Enter
+      e.key === "Backspace" || // Backspace
+      e.key === "Delete" // Delete
+    ) {
+      setHeightToggle((prev) => !prev);
+    }
+  };
+
+  const handleMessageSubmit = () => {
+    if (!commentText.trim()) return;
+    const newComment = {
+      user: { _id: user._id, firstName: user.firstName, lastName: user.lastName, profilePicture: user.profilePicture },
+      text: commentText.trim(),
+      timestamp: new Date(),
+    };
+    setWorkoutFeedback((prev) => ({
+      ...prev,
+      comments: [...prev.comments, newComment],
+    }));
+    setCommentText("");
+  };
+
+
+  return (
+    <Grid container size={12} component={Paper} spacing={1} sx={{ backgroundColor: '#282828', padding: '15px 7.5px', }}>
+      <Grid container size={12}>
+        <Typography>Overall Workout Feedback:</Typography>
+      </Grid>
+      <Grid container size={12} sx={{ padding: "0px 15px" }}>
+        <Typography sx={{ mb: 1 }}>Rate Difficulty:</Typography>
+        <RadioGroup
+          row
+          value={feedbackDifficulty}
+          onChange={handleFeedbackDifficultyChange}
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            flexWrap: { xs: "nowrap", sm: "nowrap" },
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <FormControlLabel
+            value={0}
+            control={
+              <Radio
+                sx={{
+                  "&.Mui-checked": {
+                    color: "secondary.main",
+                  },
+                }}
+              />
+            }
+            label={
+              <Typography
+                sx={{
+                  color: feedbackDifficulty === 0 ? "secondary.main" : "text.primary",
+                }}
+              >
+                Too Easy
+              </Typography>
+            }
+          />
+          <FormControlLabel
+            value={1}
+            control={
+              <Radio
+                sx={{
+                  "&.Mui-checked": {
+                    color: "primary.main",
+                  },
+                }}
+              />
+            }
+            label={
+              <Typography
+                sx={{
+                  color: feedbackDifficulty === 1 ? "primary.main" : "text.primary",
+                }}
+              >
+                Perfect
+              </Typography>
+            }
+          />
+          <FormControlLabel
+            value={2}
+            control={
+              <Radio
+                sx={{
+                  "&.Mui-checked": {
+                    color: "#d32f2f",
+                  },
+                }}
+              />
+            }
+            label={
+              <Typography
+                sx={{
+                  color: feedbackDifficulty === 2 ? "#d32f2f" : "text.primary", // custom red
+                }}
+              >
+                Too Hard
+              </Typography>
+            }
+          />
+        </RadioGroup>
+      </Grid>
+      {workoutFeedback.comments.map(comment => {
+        return (
+          <Grid container size={12} sx={{ padding: '7.5px 0', }}>
+            <Grid container size={2} sx={{ justifyContent: 'center', }}>
+              <Avatar src={comment.user.profilePicture ? `${serverURL}/user/profilePicture/${comment.user.profilePicture}` : null} />
+            </Grid>
+            <Grid container size={8} spacing={0} >
+              <Grid container size={12} spacing={1}>
+                <Typography variant="body2" display="inline">
+                  {comment.user.firstName}{" "}{comment.user.lastName}
+                </Typography>
+                <Typography
+                  variant="subtitle2"
+                  display="inline"
+                  sx={{
+                    opacity: ".33",
+                  }}
+                >
+                  {dayjs(comment.timestamp).format("MM/DD/YYYY h:mm:ss A")}
+                </Typography>
+              </Grid>
+              <Grid container size={12}>
+                <Typography variant="body1" display="block">
+                  {comment.text}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        )
+      })}
+      <Grid container size={12} sx={{ padding: "0px 15px" }}>
+        <TextField
+          label="Add comment..."
+          value={commentText}
+          onKeyDown={handleFeedbackKeyDown}
+          onChange={(e) => setCommentText(e.target.value)}
+          fullWidth
+          multiline
+          slotProps={{
+            input: {
+              endAdornment: (
+                <Button variant="contained" onClick={handleMessageSubmit}>
+                  Submit
+                </Button>
+              ),
+            },
+          }}
+        />
+
+      </Grid>
+    </Grid>
+  );
+};
 
 const FeedbackExerciseInput = ({ exercise }) => {
   const [feedbackDifficulty, setFeedbackDifficulty] = useState(exercise.feedback.difficulty);
