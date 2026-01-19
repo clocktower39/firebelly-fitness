70cc2364af64a4db5881a2d720bdabfd64a0ec4e
Matt <clocktower39@aol.com>
2026-01-19 00:03:22 -0700

    yearly calendar

--- firebelly-client/src/Redux/actions.jsx
+++ firebelly-client/src/Redux/actions.jsx
@@ -574,6 +574,47 @@ export function requestWorkoutsByMonth(date, client) {
   };
 }
 
+export function requestWorkoutsByYear(year, client) {
+  return async (dispatch) => {
+    const bearer = `Bearer ${localStorage.getItem("JWT_AUTH_TOKEN")}`;
+
+    const endpoint = "workoutYear";
+    const payload = JSON.stringify({
+      year,
+      client,
+    });
+
+    const response = await fetch(`${serverURL}/${endpoint}`, {
+      method: "post",
+      body: payload,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: bearer,
+      },
+    });
+    const data = await response.json();
+
+    data.workouts.map((workout) =>
+      workout.training.map((set) => {
+        set.map((exercise) => {
+          if (!exercise.achieved.weight) {
+            exercise.achieved.weight = [0];
+          }
+          return exercise;
+        });
+        return set;
+      })
+    );
+
+    return dispatch({
+      type: EDIT_WORKOUTS,
+      workouts: [...data.workouts],
+      user: data.user,
+      accountId: data.user._id,
+    });
+  };
+}
+
 // Creates new daily training workouts
 export function createTraining({ training, user }) {
   return async (dispatch) => {
